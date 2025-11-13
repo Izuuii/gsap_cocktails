8b14c643b164ac663991f71d0a78eadde6de2532
Izuuii <denverdeleon22@gmail.com>
2025-11-13 20:42:01 +0800

    Implement hero scroll animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,21 @@
-import { useGSAP } from '@gsap/react'
-import { SplitText } from 'gsap/all'
-import React from 'react'
-import gsap from 'gsap'
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
+import { useRef, useState, useEffect } from "react";
 
 const Hero = () => {
+    const videoRef = useRef();
+    const [isMobile, setIsMobile] = useState(
+        typeof window !== 'undefined' ? window.matchMedia('(max-width: 767px)').matches : false
+    );
+
+    useEffect(() => {
+        const mediaQuery = window.matchMedia('(max-width: 767px)');
+        const handleChange = (e) => setIsMobile(e.matches);
+        
+        mediaQuery.addEventListener('change', handleChange);
+        return () => mediaQuery.removeEventListener('change', handleChange);
+    }, []);
 
     useGSAP(() => {
         const heroSplit = new SplitText('.title', {type: 'chars, words'})
@@ -37,7 +49,30 @@ const Hero = () => {
         })
         .to('.right-leaf', { y: 200 }, 0)
         .to('.left-leaf', { y: -200 }, 0)
-    }, [])
+        
+
+        const startValue = isMobile ? 'top 50%' : 'center 60%';
+        const endValue = isMobile ? '120% top' : 'bottom top';
+
+        let tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'video',
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+            },
+        });
+    
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration
+            });
+        }
+
+    }, [isMobile])
+    
+
     
   return (
     <>
@@ -68,6 +103,10 @@ const Hero = () => {
                 </div>
             </div>
         </section>
+
+        <div className='video-absolute inset-0'>
+            <video src="/videos/output.mp4" ref={videoRef} muted playsInline preload='auto'  />
+        </div>
     </>
   )
 }
